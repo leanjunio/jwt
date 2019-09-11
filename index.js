4a8322a3c0e29db7a9b17550c1f721cd0aea9ac5
Lean <leanjunio@live.com>
2019-09-11 14:17:59 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Import Routes
+const authRoute = require('./routes/auth');
+
+// Connect to DB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true } ,() => console.log(`Connected to DB!`));
+
+// Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+// parse application/json
+app.use(bodyParser.json());
+
+// Route middlewares
+app.use('/api/user', authRoute);
+
+
+app.listen(3000, () => console.log(`Listening on port ${3000}`));
