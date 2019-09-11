616112c2e393106e99fae62346c685118e714a53
Lean <leanjunio@live.com>
2019-09-11 16:24:05 -0400

    has password with bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 const User = require('../model/User');
 
 const { registerValidation } = require('../validation');
@@ -16,12 +17,16 @@ router.post('/register', async (req, res) => {
   if (emailExists) {
     return res.status(400).send(`Email already exists`);
   }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
   
   // Create a new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: hashedPassword
   });
 
   // Attempt to save the created user to DB
